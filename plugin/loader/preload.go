fe5c7266322dcfde88ea78e99e5095395845fe48
riteshRcH <ritesht93@gmail.com>
2022-05-04 09:22:22 -0700

    removed plugins: badgerds, dagjose, git

--- plugin/loader/preload.go
+++ plugin/loader/preload.go
@@ -1,10 +1,7 @@
 package loader
 
 import (
-	pluginbadgerds "github.com/ipfs/go-ipfs/plugin/plugins/badgerds"
-	pluginiplddagjose "github.com/ipfs/go-ipfs/plugin/plugins/dagjose"
 	pluginflatfs "github.com/ipfs/go-ipfs/plugin/plugins/flatfs"
-	pluginipldgit "github.com/ipfs/go-ipfs/plugin/plugins/git"
 	pluginlevelds "github.com/ipfs/go-ipfs/plugin/plugins/levelds"
 	pluginpeerlog "github.com/ipfs/go-ipfs/plugin/plugins/peerlog"
 )
@@ -14,9 +11,6 @@ import (
 // To change it, modify the plugin/loader/preload.sh
 
 func init() {
-	Preload(pluginipldgit.Plugins...)
-	Preload(pluginiplddagjose.Plugins...)
-	Preload(pluginbadgerds.Plugins...)
 	Preload(pluginflatfs.Plugins...)
 	Preload(pluginlevelds.Plugins...)
 	Preload(pluginpeerlog.Plugins...)
